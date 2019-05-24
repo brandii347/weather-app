c5680768529dbb12879eac62e7709a7c125e8fe2
Brandi Chavez <brandi.nicole22@gmail.com>
2019-05-23 18:55:39 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -62,11 +62,11 @@ class App extends React.Component {
                     <div className="main">
                         <div className="container">
                             <div className="row">
-                                <div className="col-xs-5 title-container">
+                                <div className="col-5 title-container">
                                     <Titles />
                                 
                                 </div>
-                                <div className="col-xs-7 form-container">
+                                <div className="col-7 form-container">
                                     <Form getWeather={this.getWeather}/>
                                     <Weather 
                                         temperature={this.state.temperature}
